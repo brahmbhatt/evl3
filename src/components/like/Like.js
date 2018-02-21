95d44e77aa37e632ffade790f3b456fc98505691
brahmbhatt <margibrahmbhatt13@gmail.com>
2018-02-21 13:50:35 +0530

    like functionality half done

--- src/components/like/Like.js
+++ src/components/like/Like.js
@@ -2,27 +2,46 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './Like.css';
 
-export default class Like extends React.Component {
+class Like extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: 'Like-button',
+    };
+  }
   likeBook = () => {
     fetch(`http://localhost:3000/books/like/${this.props.id}`, {
       method: 'POST',
     }).then(response => response.json())
       .then(data => console.log(data));
   }
+  changeButtonColor = () => {
+    for (let i = 0; i < this.props.likesArray.length; i += 1) {
+      if (this.props.likesArray[i].bookId === this.props.id) {
+        if (this.props.likesArray[i].like === 'true') {
+          console.log('yes it is true');
+
+          this.setState({ color: 'redButton' });
+        }
+      }
+    }
+  }
   render() {
     return (
       <button
-        className="Like-button"
+
+        className={this.state.color}
         onClick={(event) => {
         this.likeBook();
         event.target.className = 'redButton';
       }}
-      />
+      >{this.changeButtonColor}
+      </button>
     );
   }
 }
-// const mapStateToProps = state => ({
-//   booksArray: state.reducer.booksArray,
-//   likesArray: state.reducer.likesArray,
-// });
-// export default connect(mapStateToProps, null)(BookPageBody);
+const mapStateToProps = state => ({
+  booksArray: state.reducer.booksArray,
+  likesArray: state.reducer.likesArray,
+});
+export default connect(mapStateToProps, null)(Like);
